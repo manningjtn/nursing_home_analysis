afb9a1a4bf31ce568c6b2f0683ce5cd798b2cf73
John Manning <manningjbkk@gmail.com>
2026-02-01 18:40:20 +0000

    correlation model sync

--- macros/correlation_between_columns.sql
+++ macros/correlation_between_columns.sql
@@ -1,19 +1,20 @@
-{% macro correlation(schema, table, col_a, col_b) %}
-with
-avg_mean as (
+{% macro correlation(col_a, col_b) %}
+with avg_mean as (
 	select 
-		avg({{col_a}}) as avg_val,
-		avg({{col_b}}) as avg_goal
-	from {{schema}}.{{table}} t
-)
-, math_prep as (
+		avg(t.{{col_a}}) as avg_val,
+		avg(t.{{col_b}}) as avg_goal
+	from {{source("urban_rural", "CMS_PROVIDER")}} as t
+),
+
+math_prep as (
 	select
-		({{col_a}}-avg_val) as val_dist, 
-		pow({{col_a}}-avg_val,2) as val_dist_sq, 
-		({{col_b}}-avg_goal) as goal_dist, 
-		pow({{col_b}}-avg_goal-avg_goal,2) as goal_dist_sq
-	from {{schema}}.{{table}} t, avg_mean a
+		(t.{{col_a}}-avg_val) as val_dist, 
+		pow(t.{{col_a}}-avg_val,2) as val_dist_sq, 
+		(t.{{col_b}}-avg_goal) as goal_dist, 
+		pow(t.{{col_b}}-avg_goal-avg_goal,2) as goal_dist_sq
+	from {{source("urban_rural", "CMS_PROVIDER")}} as t, avg_mean a
 )
+
 select 
 	(
 		sum(val_dist*goal_dist)
---
+++ models/cms_nursing_home_analysis/nursing_hours_correlation.sql
@@ -0,0 +1,7 @@
+select  
+    {{correlation('REPORTED_NURSE_AIDE_STAFFING_HOURS_PER_RESIDENT_PER_DAY', 'STAFFING_RATING')}} as nurse_aide_staffing
+
+
+
+
+
